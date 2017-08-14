41e36f3961df0d1b9d9a22513ec0a34cbe709a0b
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-08-14 19:17:43 +0200

    render DOM

--- async.js
+++ async.js
@@ -1,6 +1,40 @@
+const refreshButtton = document.querySelector('.refresh');
+
+
 const requestStream = Rx.Observable.of('https://api.github.com/users');
 
 const responseStream = requestStream
   .flatMap(requestUrl => Rx.Observable.fromPromise(jQuery.getJSON(requestUrl)));
 
 responseStream.subscribe(response => console.log(response));
+
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser => {
+    return listUser[Math.floor(Math.random() * listUser.length)];
+  });
+}
+
+const suggestion1Stream = createSuggestionStream(responseStream);
+const suggestion2Stream = createSuggestionStream(responseStream);
+const suggestion3Stream = createSuggestionStream(responseStream);
+
+function renderSuggestion(userData, selector) {
+  const element = document.querySelector(selector);
+
+  const usernameEl = element.querySelector('.username');
+  usernameEl.href = userData.html_url;
+  usernameEl.textContent = userData.login;
+
+  const imgEl = element.querySelector('img');
+  imgEl.src = userData.avatar_url;
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
